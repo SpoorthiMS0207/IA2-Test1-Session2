8cd6c3850ef22f3ff64eb8b15514082669117d8f
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-02-12 19:13:18 +0000

    debugging of p1

--- p8final.c
+++ p8final.c
@@ -56,7 +56,7 @@ void output(int n, Triangle t[n], Triangle smallest)
   {
     printf("%f, %f and\n", t[i].base, t[i].altitude);
   }
-  printf("%f, %f\nis\ntriangle with base = %f and altitude = %f having area = %f\n", t[n-1].base, t[n-1].altitude, smallest.base, smallest.altitude, smallest.area);
+  printf("The smallest amonf %f, %f\nis\ntriangle with base = %f and altitude = %f having area = %f\n", t[n-1].base, t[n-1].altitude, smallest.base, smallest.altitude, smallest.area);
 }
 int main()
 {
